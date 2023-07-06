a855df22437a5abc2e4f267a3446d990285301b8
bocktoberfest <sbock07+github@gmail.com>
2023-07-06 16:27:29 +0000

    removed jQuery dependency

--- admin_shortcut_src.js
+++ admin_shortcut_src.js
@@ -1,15 +1,8 @@
-$.ajax({
-    type: "GET",
-    url: location.protocol + "//" + location.host + location.pathname + ".json",
-    cache: false,
-    success: function(data){
-        console.log(data);
-        if(data.product){
-            var admin_url = "https://"+window.location.hostname+"/admin/products/"+data.product.id;
-            window.open(admin_url,'_blank');
-        }else if(data.collection){
-            var admin_url = "https://"+window.location.hostname+"/admin/collections/"+data.collection.id;
-            window.open(admin_url,'_blank');
-        }
+javascript: fetch(location.pathname + '.json') .then(resp => resp.json()).then(data => {
+    const prefix = `https://${location.hostname}/admin`
+    if (data.product) {
+      window.open(`${prefix}/products/${data.product.id}`, '_blank');
+    } else if (data.collection) {
+      window.open(`${prefix}/collections/${data.collection.id}`, '_blank');
     }
-});
+  });
